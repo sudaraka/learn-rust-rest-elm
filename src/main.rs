7e1ebd6e4a036d44e31932893c1e21abc8b987f8
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2018-03-08 12:53:34 +0530

    Launch rocket web server with DB pool in it

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate diesel;
 extern crate dotenv;
 extern crate r2d2;
 extern crate r2d2_diesel;
+extern crate rocket;
 
 use dotenv::dotenv;
 use std::env;
@@ -11,22 +12,15 @@ mod schema;
 mod models;
 mod db;
 
-fn main() {
+fn rocket() -> rocket::Rocket {
     dotenv().ok();
 
     let db_url = env::var("DATABASE_URL").expect("Please set DATABASE_URL");
     let pool = db::init_pool(db_url);
-    let con = pool.get().unwrap();
 
-    let book = models::NewBook {
-        title: String::from("Gravity's Rainbow"),
-        author: String::from("Thomas Pynchon"),
-        published: true,
-    };
+    rocket::ignite().manage(pool)
+}
 
-    if models::Book::insert(book, &con) {
-        println!("Success!");
-    } else {
-        println!("Failed :(");
-    }
+fn main() {
+    rocket().launch();
 }
